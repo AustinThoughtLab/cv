2b08945a399e88786360a65d124d4018eac598c0
Kyle MacDonald <kylem412@gmail.com>
2019-09-10 18:09:49 -0700

    updating cv deploy for new site dir

--- deploy_cv.sh
+++ deploy_cv.sh
@@ -1,9 +1,9 @@
 # copy updated cv to website 
-cp km_cv.pdf ../kemacdonald.github.io/materials/
+cp km_cv.pdf ../kemacdonald.github.io/assets/pdfß
 
 # get commit message from user
 read -p "Enter a commit message: "  commit_msg
 
 cd ../kemacdonald.github.io
 git add -A; git commit -m "$commit_msg"; git push origin master
-echo "Copied CAT experiment to kemacdonald.github.io and pushed to git"
+echo "Copied CV to kemacdonald.github.io and pushed to git"
