1201ba9a1a3bdc84f1f6fffbdfd3808ec2247050
Kyle MacDonald <kylem412@gmail.com>
2019-09-10 18:12:47 -0700

    changing remote to source

--- deploy_cv.sh
+++ deploy_cv.sh
@@ -1,9 +1,9 @@
 # copy updated cv to website 
-cp km_cv.pdf ../kemacdonald.github.io/assets/pdfß
+cp km_cv.pdf ../kemacdonald.github.io/assets/pdf
 
 # get commit message from user
 read -p "Enter a commit message: "  commit_msg
 
 cd ../kemacdonald.github.io
-git add -A; git commit -m "$commit_msg"; git push origin master
+git add -A; git commit -m "$commit_msg"; git push origin source
 echo "Copied CV to kemacdonald.github.io and pushed to git"
